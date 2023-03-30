086c9e6ac89ff8ce62bc1450efed87d661eb3f35
Nathan <89826214+npiecyk@users.noreply.github.com>
2023-03-29 20:34:39 -0400

    Added projects.html and improved UI

--- main_js.js
+++
@@ -1,9 +0,0 @@
-function showEmail(){ //avoid bots by prompting the user to answer a question before displaying my email
-    var result = prompt("What is 1 + 3?");
-    if(result === '4'){
-        alert("npiecyk@nd.edu")
-    }
-    else{
-        alert("Sorry, try again!")
-    }
-}
